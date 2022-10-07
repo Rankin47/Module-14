5bf5719f000e68958ec828e144a772035604a3c1
Rankin47 <samuelcarterrankin@gmail.com>
2022-10-07 19:28:25 -0400

    First Commit

---
+++ js/comment.js
@@ -0,0 +1,28 @@
+const commentFormHandler = async function(event) {
+    event.preventDefault();
+        const blog_id = document.querySelector('.new-comment-form').dataset.blogid;
+    const comment_description = document.querySelector('#comment_description').value.trim();
+    if (comment_description) {
+      console.log("testing post for comments")
+      await fetch('/apis/comments', {
+        method: 'POST',
+        body: JSON.stringify({
+          blog_id,
+          comment_description
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      console.log("post for comments compleat")
+      document.location.reload();
+    }
+  };
+  document
+    .querySelector('.new-comment-form')
+    .addEventListener('submit', commentFormHandler);
+  
+    
+  
+  
+  
